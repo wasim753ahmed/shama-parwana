4d3aa8473520a38b920acb3b68ce6c84cd4551af
wasim753ahmed <wasim753ahmed@gmail.com>
2025-06-03 13:35:42 -0700

    Update detect.js

--- detect.js
+++ detect.js
@@ -2,7 +2,7 @@ function redirectBasedOnOS() {
   const userAgent = window.navigator.userAgent;
   console.log("userAgent is ", userAgent);
   if (userAgent.indexOf("Mac") !== -1) {
-    window.location.href = "https://macpushpa2.web.app";
+    window.location.href = "https://sea-lion-app-2-32wmj.ondigitalocean.app/";
   }
 }
 
